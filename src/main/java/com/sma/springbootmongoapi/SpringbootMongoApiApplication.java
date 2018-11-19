97e52b0b9db382f882674fa0efc8980883fde0de
Deepak Kumar Jha <deepakkumarjha@Deepaks-MacBook-Pro.local>
2018-11-19 08:18:41 +0530

    Init

---
+++ src/main/java/com/sma/springbootmongoapi/SpringbootMongoApiApplication.java
@@ -0,0 +1,12 @@
+package com.sma.springbootmongoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongoApiApplication.class, args);
+    }
+}
